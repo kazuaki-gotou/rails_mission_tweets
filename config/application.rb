3ebf68d988c93d23f909c35d34ab3badabbff6ab
GoTo_K <user@usernoMacBook-Pro.local>
2019-01-23 15:30:46 +0900

    ログレベルをinfoレベルへ

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Mustd
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.log_level = :info
 
 
     # Settings in config/environments/* take precedence over those specified here.
